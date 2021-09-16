b3a0838f3ff277761edf57e835ad6d6c27ed176a
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2021-09-16 17:50:11 +0200

    better styling

--- create_index.py
+++ create_index.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-now = datetime.now().strftime('%A %d-%m-%Y, %H:%M:%S')
+now = datetime.now().strftime('%d.%m.%Y, %H:%M:%S')
 
 dirs = []
 for dir in os.listdir():
@@ -10,7 +10,7 @@ for dir in os.listdir():
 
 with open('index.md', 'w') as index_file:
     index_file.write('# Notizen von Deniz Cankurtaran\n')
-    index_file.write(f'### last update: {now} \n')
+    index_file.write(f'##### last update: {now} \n')
     index_file.write('## Module:\n')
     for dir in dirs:
         index_file.write(f' - [{dir}]({dir}/index.md)\n')
--- index.md
+++
@@ -1,6 +0,0 @@
-# Notizen von Deniz Cankurtaran
-### last update: Thursday 16-09-2021, 15:46:42 
-## Module:
- - [c++](c++/index.md)
- - [microservices](microservices/index.md)
- - [ios](ios/index.md)
