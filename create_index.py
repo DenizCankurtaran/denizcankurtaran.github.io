d3960624439fe5e4c83176d96ed2ed71d7008606
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2021-09-16 17:46:25 +0200

    update

--- create_index.py
+++ create_index.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime
+
+now = datetime.now().strftime('%A %d-%m-%Y, %H:%M:%S')
 
 dirs = []
 for dir in os.listdir():
@@ -7,8 +10,7 @@ for dir in os.listdir():
 
 with open('index.md', 'w') as index_file:
     index_file.write('# Notizen von Deniz Cankurtaran\n')
-    index_file.write('## Module:')
+    index_file.write(f'### last update: {now} \n')
+    index_file.write('## Module:\n')
     for dir in dirs:
         index_file.write(f' - [{dir}]({dir}/index.md)\n')
-
-    
